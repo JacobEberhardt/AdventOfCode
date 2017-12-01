07bc0fb61c4af3a5b0d859e3f0c6322895b1b13a
Jacob Eberhardt <jacob.eberhardt@tu-berlin.de>
2017-12-01 15:52:11 +0100

    Added part two of day 1.

--- src/day1.rs
+++ src/day1.rs
@@ -1,10 +1,25 @@
-pub fn calculate_reverse_captcha(input: &str ) -> u32 {
+pub fn calculate_reverse_captcha_star1(input: &str ) -> u32 {
     let mut sum = 0;
 
     let chars:Vec<char> = input.chars().collect();
 
     for i in 0..chars.len() {
         if chars[i] == chars[ (i+1) % chars.len()] {
+            sum += chars[i].to_digit(10).unwrap();
+        } 
+    }
+    sum
+}
+
+pub fn calculate_reverse_captcha_star2(input: &str ) -> u32 {
+    let mut sum = 0;
+
+    let chars:Vec<char> = input.chars().collect();
+    assert!(chars.len() % 2 == 0); // input length is even
+    let offset = chars.len()/2;
+
+    for i in 0..chars.len() {
+        if chars[i] == chars[ (i+offset) % chars.len()] {
             sum += chars[i].to_digit(10).unwrap();
         } 
     }
